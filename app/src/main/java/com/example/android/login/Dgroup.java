9449617f41834e596c0afef0c36ef15cb9622ff2
manjusha23 <manjushapednekar23@gmail.com>
2015-10-22 17:44:55 +0530

    Create Dgroup.java

---
+++ app/src/main/java/com/example/android/login/Dgroup.java
@@ -0,0 +1,21 @@
+public class Dgroup
+{
+	private String _groupName;
+	
+	public Dgroup()
+	{}
+	public Dgroup(String group)
+	{
+		_groupName = group;
+	}
+	
+	public void set_groupName(String group)
+	{
+		_groupName = group;
+	}
+	
+	public String get_groupName()
+	{
+		return _groupName;
+	}
+}
